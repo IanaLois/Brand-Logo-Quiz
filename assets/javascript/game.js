c2899bfb09938195b5b1b985ef9dc959da6ccf8a
IanaLois <ianaesteron@gmail.com>
2023-03-09 22:47:21 +0000

    Add extra questions and options in game javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,8 @@ const questionProgress = document.querySelector('#questionProgress');
 const scoreText = document.querySelector('#score');
 const questionProgressBarFull = document.querySelector('#questionProgressBarFull');
 
-const scorePoints = 10;
-const maxQuestions = 5;
+const scorePoints = 25;
+const maxQuestions = 8;
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -16,7 +16,7 @@ let availableQuestions = [];
 
 let questions = [
     {
-        question: 'https://media.graphassets.com/PdRpmfEjRWyXsmF3Z4Ex',
+        question: 'assets/images/logos/question-1-logo.png',
         choice1: 'Dickies',
         choice2: 'Carhartt',
         choice3: 'North Face',
@@ -24,35 +24,59 @@ let questions = [
         answer: 2,
     },
     {
-        question: 'https://pbs.twimg.com/profile_images/1628100176437968910/pCKhYh2f_400x400.jpg',
-        choice1: '1',
-        choice2: '6',
-        choice3: '5',
-        choice4: '4',
-        answer: 2,
-    },
-    {
-        question: 'What is 6 + 2?',
-        choice1: '8',
-        choice2: '3',
-        choice3: '5',
-        choice4: '4',
-        answer: 1,
-    },
-    {
-        question: 'What is 8 + 2?',
-        choice1: '1',
-        choice2: '3',
-        choice3: '10',
-        choice4: '4',
+        question: 'assets/images/logos/question-2-logo.jpg',
+        choice1: 'Yamaha',
+        choice2: 'Gibson',
+        choice3: 'Fender',
+        choice4: 'Heritage',
         answer: 3,
     },
     {
-        question: 'What is 0 + 2?',
-        choice1: '2',
-        choice2: '3',
-        choice3: '5',
-        choice4: '4',
+        question: 'assets/images/logos/question-3-logo.png',
+        choice1: 'Guess',
+        choice2: 'Prada',
+        choice3: 'Coach',
+        choice4: 'Versace',
+        answer: 3,
+    },
+    {
+        question: 'assets/images/logos/question-4-logo.jpg',
+        choice1: 'Nintendo',
+        choice2: 'Steam',
+        choice3: 'Xbox',
+        choice4: 'Playstation',
+        answer: 4,
+    },
+    {
+        question: 'assets/images/logos/question-5-logo.jpg',
+        choice1: 'Jollibee',
+        choice2: 'Five Guys',
+        choice3: 'KFC',
+        choice4: 'Greggs',
+        answer: 1,
+    },
+    {
+        question: 'assets/images/logos/question-6-logo.jpg',
+        choice1: 'Slack',
+        choice2: 'Discord',
+        choice3: 'Skype',
+        choice4: 'Viber',
+        answer: 2,
+    },
+    {
+        question: 'assets/images/logos/question-7-logo.jpg',
+        choice1: 'Birkenstock',
+        choice2: 'Dr. Martens',
+        choice3: 'Converse',
+        choice4: 'Vans',
+        answer: 4,
+    },
+    {
+        question: 'assets/images/logos/question-8-logo.png',
+        choice1: 'Dunkin\' Donuts',
+        choice2: 'Krispy Kreme',
+        choice3: 'Donut King',
+        choice4: 'Tim Hortons',
         answer: 1,
     }
 ];
